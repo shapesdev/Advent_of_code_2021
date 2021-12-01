caac1628d67dfacf90f21be3ec4fd159449f452d
Shapsas <donatas.vaiciulis1@gmail.com>
2021-12-01 21:10:14 +0200

    Project creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advent_of_code_2021
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            
+            Console.ReadLine();
+        }
+    }
+}
